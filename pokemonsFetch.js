d5ed96f8be3c46846d4262472fcaf3c207a44a3f
Denys <denyshcmid@gmail.com>
2020-11-30 16:47:30 -0300

    Criando paginação de 20 em 20 pokemons

--- pokemonsFetch.js
+++ pokemonsFetch.js
@@ -2,75 +2,99 @@
 //URL API POKEMON "https://pokeapi.co/"
 
 const container = document.querySelector('.container');
+const next = document.querySelector('.next');
+const previous = document.querySelector('.previous');
+
+let offset = 0;
+let maxPokemon = 20;
+let limit = 0;
 
 const colors = {
-	fire: '#FDDFDF',
-	grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
 };
 
-const creatElementPokemon = async ()=>{
-
-    const maxPokemons = 60;
-
-    for (let i = 1; i <= maxPokemons; i++) {
-        let idPokemon = i;
-        let descriptionPokemon = ''
+const getPokemonsApi = async () => {
     
-        await fetch(`https://pokeapi.co/api/v2/pokemon-species/${idPokemon}`)
+    for(let i = 1; i < maxPokemon; i++) {
+        let descriptionPokemon = ''
+        await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}limit=${limit}`, {
+            method: 'GET',
+            'content-type': 'application-json'
+        })
         .then(response => response.json())
         .then(data => {
-            // console.log(data.flavor_text_entries[idPokemon])
-            descriptionPokemon = data.flavor_text_entries[idPokemon].flavor_text
-        })
-    
-        await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
-        .then(response => response.json())
-        .then(data => {            
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${i}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(`https://pokeapi.co/api/v2/pokemon-species/${i}`)
+                descriptionPokemon = data.flavor_text_entries[i].flavor_text
+                console.log(data.flavor_text_entries[i])
+            });
             
-            let type1 = data.types[0].type.name
-            let type2 = ''
-            data.types[1] === undefined ? type2 = '' : type2 = data.types[1].type.name
-            let types = ''
-            type2 === '' ? types = type1 : types = type1 + ' | ' + type2
-            
-            const color = colors[type1]
-            const pokemon = document.createElement('div')
-            pokemon.classList.add('pokemon')
-            pokemon.style.backgroundColor = color
+            fetch(`https://pokeapi.co/api/v2/pokemon/${data.results[i-1].name}`)
+            .then(response => response.json())
+            .then(data => {
+                
+                let type1 = data.types[0].type.name
+                let type2 = ''
+                data.types[1] === undefined ? type2 = '' : type2 = data.types[1].type.name
+                let types = ''
+                type2 === '' ? types = type1 : types = type1 + ' | ' + type2
+                        
+                const color = colors[type1]
+                const pokemon = document.createElement('div')
+                pokemon.classList.add('pokemon')
+                pokemon.style.backgroundColor = color
 
-            const createPokemon =  `
+                const createPokemon = `
                 <h2 class="pokemon__name">${data.name}</h2>
                 <div class="pokemon__img">
-                    <img src = "https://pokeres.bastionbot.org/images/pokemon/${idPokemon}.png" alt="${data.name}"/>
+                    <img src = "https://pokeres.bastionbot.org/images/pokemon/${data.id}.png" alt="${data.name}"/>
                 </div>
                 <p class="pokemon__description">${descriptionPokemon}</p>
                 <p class= "pokemon__type">Type: ${types}</p>
-                <span class="pokemon__id">${idPokemon}</span>
-            `;    
-            
-            pokemon.innerHTML = `${createPokemon} ` 
-            container.appendChild(pokemon)        
-        });
-    }   
+                <span class="pokemon__id">${data.id}</span>
+                `;
+                
+                
+                pokemon.innerHTML = `${createPokemon} `
+                container.appendChild(pokemon)
+            });
+        })
+    }
 }
 
-creatElementPokemon();
+getPokemonsApi();
+
+next.addEventListener('click', () => {
+
+    container.innerHTML = ''
+    offset += 20;
+    limit += 20;
+    getPokemonsApi();
+})
 
 
-// fetch('https://pokeapi.co/api/v2/pokemon/?limit=20')
-// .then(response => response.json())
-// .then(data => {
-//     console.log(data)
-// })
+previous.addEventListener('click', () => {
+    if (offset >= 20) {
+
+        container.innerHTML = ''
+        offset -= 20;
+        limit -= 20;
+        getPokemonsApi();
+    }
+
+})
